7dae28527c16e9b5a58b5947d10d593e1c25d64f
kyoungmin0 <dgistkm@gmail.com>
2022-11-13 21:40:24 +0100

    fix: modify the attack to just once

--- src/Enemy.java
+++ src/Enemy.java
@@ -9,6 +9,8 @@ public class Enemy {
 
     int y;
 
+    int n = 0;
+
     public Enemy(ImageObject imageObj, int damage, int x, int y) {
         this.imageObj = imageObj;
         this.damage = damage;
@@ -20,12 +22,17 @@ public class Enemy {
         x -= 8;
         if (x < -40) {
             x = 900;
+            n = 0;
         }
     }
 
     public void crash(Character character) {
         if (Collision.isCollided(this.x, this.y, character.x, character.y, this.imageObj.getWidth(), this.imageObj.getHeight(), character.imageObj.getWidth(), character.imageObj.getHeight())) {
-            character.hp = character.hp - damage < 0 ? 0 : character.hp - damage;
+            if (n == 0) {
+                character.hp = character.hp - damage < 0 ? 0 : character.hp - damage;
+                n += 1;
+            }
+
 
             new Thread(new Runnable() {
                 @Override
