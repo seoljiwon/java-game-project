00d1b90d926a6ea43d53bb12280cbbd7522e5d9e
seoljiwon <suljiwon@naver.com>
2022-11-13 22:22:44 +0900

    feat: implement character jump slide

--- src/Character.java
+++ src/Character.java
@@ -1,30 +1,63 @@
-import java.awt.*;
-
 public class Character {
-    Image image;
+    ImageObject imageObj;
     int hp;
     int speed;
+    int jumpScale = 40;
 
     // character coordinates
-    int x = 100;
-    int y = 365;
+    int x;
+    int y;
 
-    public Character(Image image, int hp, int speed){
-        this.image = image;
+    public Character(ImageObject imageObj, int hp, int speed) {
+        this.imageObj = imageObj;
         this.hp = hp;
         this.speed = speed;
+
+        init();
     }
 
-    public void jump(){
-        y -= 5;
+    public void init() {
+        x = 100;
+        y = 365;
+        imageObj.image = imageObj.getImage(imageObj.path);
     }
 
-    public void slide(){
-        x += 2;
+    public void jump(int jumpScale) {
+        y -= jumpScale;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                }
+
+                init();
+            }
+
+        }).start();
     }
 
-    public void move(KeyEventListener keyEventListener){
-        if (keyEventListener.keyUp == true) jump();
+    public void slide() {
+        y += 5;
+        imageObj.image = imageObj.image.getScaledInstance(imageObj.width, imageObj.height / 2, imageObj.hints);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                }
+
+                init();
+            }
+
+        }).start();
+    }
+
+    public void move(KeyEventListener keyEventListener) {
+        if (keyEventListener.keyUp == true) jump(jumpScale);
         if (keyEventListener.keyDown == true) slide();
     }
 }
