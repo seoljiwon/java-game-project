728698b6ba88e369eb2493e327aaf19988dab94f
seoljiwon <suljiwon@naver.com>
2022-11-14 04:52:19 +0900

    fix: fix image path

--- src/BombEnemy.java
+++ src/BombEnemy.java
@@ -1,6 +1,7 @@
 import coordinate.ImageObject;
+
 public class BombEnemy extends Enemy {
-    ImageObject boomImg = new ImageObject("src/image/boomm.png", 40, 40);
+    ImageObject boomImg = new ImageObject("image/boomm.png", 40, 40);
 
     public BombEnemy(ImageObject imageObj, int damage, int x, int y) {
         super(imageObj, damage, x, y);
