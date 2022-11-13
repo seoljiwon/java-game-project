19c90aabe61e069c3da6f29c49eb09f7e9412394
seoljiwon <suljiwon@naver.com>
2022-11-14 06:24:40 +0900

    refactor: delete useless code, rename variable name

--- src/Enemy.java
+++ src/Enemy.java
@@ -9,7 +9,7 @@ public class Enemy {
 
     int y;
 
-    int n = 0;
+    int crashCount = 0;
 
     public Enemy(ImageObject imageObj, int damage, int x, int y) {
         this.imageObj = imageObj;
@@ -22,28 +22,14 @@ public class Enemy {
         x -= 8;
         if (x < -40) {
             x = 900;
-            n = 0;
+            crashCount = 0;
         }
     }
 
     public void crash(Character character) {
-        if (Collision.isCollided(this.x, this.y, character.x, character.y, this.imageObj.getWidth(), this.imageObj.getHeight(), character.imageObj.getWidth(), character.imageObj.getHeight())) {
-            if (n == 0) {
-                character.hp = character.hp - damage < 0 ? 0 : character.hp - damage;
-                n += 1;
-            }
-
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (Exception e) {
-                    }
-                }
-
-            }).start();
+        if (crashCount == 0 && Collision.isCollided(this.x, this.y, character.x, character.y, this.imageObj.getWidth(), this.imageObj.getHeight(), character.imageObj.getWidth(), character.imageObj.getHeight())) {
+            character.hp = character.hp - damage < 0 ? 0 : character.hp - damage;
+            crashCount += 1;
         }
     }
 }
